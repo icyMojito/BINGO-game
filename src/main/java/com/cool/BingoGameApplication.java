1d004f481f369e7a5875c68641a49d26074db18d
icyMojito <dev.icymojito@gmail.com>
2020-08-09 22:55:41 +0900

    feat: BingoGameApplication에 빙고판의 크기를 정하는 구현 추가

--- src/main/java/com/cool/BingoGameApplication.java
+++ src/main/java/com/cool/BingoGameApplication.java
@@ -1,8 +1,25 @@
 package com.cool;
 
+import com.cool.bingo.BingoSize;
+import com.cool.exception.InvalidBingoSizeException;
+import com.cool.view.InputView;
+import com.cool.view.OutputView;
+
 import java.io.IOException;
 
 public class BingoGameApplication {
     public static void main(String[] args) throws IOException {
+        BingoSize bingoSize = fixBingoSize();
+    }
+
+    private static BingoSize fixBingoSize() throws IOException {
+        while (true) {
+            OutputView.printRequestForBingoSize();
+            String playerAnswer = InputView.requestPlayerAnswer();
+            try {
+                return BingoSize.from(playerAnswer);
+            } catch (InvalidBingoSizeException e) {
+            }
+        }
     }
 }
