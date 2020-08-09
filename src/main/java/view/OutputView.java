00258ab83b46c6738762bd30428e5aef0b3456d7
icyMojito <dev.icymojito@gmail.com>
2020-08-09 18:40:19 +0900

    feat: 사용자에게 게임 진행 상황을 출력하는 OutputView 생성
    빙고판 크기 입력을 요청하는 안내문 출력 기능 생성

---
+++ src/main/java/view/OutputView.java
@@ -0,0 +1,11 @@
+package view;
+
+public class OutputView {
+
+    public static void printRequestForBingoSize() {
+        System.out.println("👼🏼 빙고판의 크기를 입력해 주세요!");
+        System.out.println("⚠️ 빙고판의 크기는 3X3부터 8X8까지 지정할 수 있으며, 숫자만 입력 가능해요.");
+        System.out.println("   예를 들어 4X4로 하고 싶으면 4만 적어주세요.");
+        System.out.println("➡ ");
+    }
+}
