e32ac32689d64850a87c2d5f35e18323949718b6
icyMojito <dev.icymojito@gmail.com>
2020-08-09 18:32:35 +0900

    feat: 사용자에게 값을 입력받을 수 있는 InputView 생성

---
+++ src/main/java/view/InputView.java
@@ -0,0 +1,17 @@
+package view;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class InputView {
+    private static final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+    public static String requestPlayerAnswer() throws IOException {
+        return bufferedReader.readLine();
+    }
+
+    public static void close() throws IOException {
+        bufferedReader.close();
+    }
+}
