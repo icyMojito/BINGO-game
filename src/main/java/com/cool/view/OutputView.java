5598c50363aa0dc49980e673131b529fd10210b1
icyMojito <dev.icymojito@gmail.com>
2020-08-09 22:40:02 +0900

    refactor: view 파일들의 소속 패키지 변경과 OutputView의 빙고판 크기 요청 출력 메서드 수정
    빙고판 크기 입력을 요청하는 출력문에 서식을 넣음

---
+++ src/main/java/com/cool/view/OutputView.java
@@ -0,0 +1,26 @@
+package com.cool.view;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class OutputView {
+    private static final BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void printRequestForBingoSize() throws IOException {
+        bufferedWriter.write("👼🏼 " + Color.MAGENTA_BOLD.fillText("빙고판의 크기") + "를 입력해 주세요!");
+        bufferedWriter.newLine();
+        bufferedWriter.write("⚠️ 빙고판의 크기는 " + Color.YELLOW_UNDERLINED.fillText("3X3부터 8X8까지")
+                                 + " 지정할 수 있으며, " + Color.BLACK_BACKGROUND.fillText("숫자만") + " 입력 가능해요.");
+        bufferedWriter.newLine();
+        bufferedWriter.write("   예를 들어 4X4로 하고 싶으면 4만 적어주세요.");
+        bufferedWriter.newLine();
+        bufferedWriter.write("➡️ ");
+
+        bufferedWriter.flush();
+    }
+
+    public static void close() throws IOException {
+        bufferedWriter.close();
+    }
+}
