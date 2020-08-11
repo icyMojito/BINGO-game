0a7ced1a3a9ae0721f7ec9a7739299d9c9d61ec8
icyMojito <dev.icymojito@gmail.com>
2020-08-11 23:47:08 +0900

    feat: BingoGameApplication에 빙고 종류를 정하는 구현 추가

--- src/main/java/com/cool/BingoGameApplication.java
+++ src/main/java/com/cool/BingoGameApplication.java
@@ -1,7 +1,9 @@
 package com.cool;
 
 import com.cool.bingo.BingoSize;
+import com.cool.bingo.BingoType;
 import com.cool.exception.InvalidBingoSizeException;
+import com.cool.exception.InvalidBingoTypeException;
 import com.cool.view.InputView;
 import com.cool.view.OutputView;
 
@@ -9,16 +11,28 @@ import java.io.IOException;
 
 public class BingoGameApplication {
     public static void main(String[] args) throws IOException {
-        BingoSize bingoSize = fixBingoSize();
+        BingoSize bingoSize = createBingoSize();
+        BingoType bingoType = createBingoType();
     }
 
-    private static BingoSize fixBingoSize() throws IOException {
+    private static BingoSize createBingoSize() throws IOException {
         while (true) {
             OutputView.printRequestForBingoSize();
             String playerAnswer = InputView.requestPlayerAnswer();
             try {
                 return BingoSize.from(playerAnswer);
             } catch (InvalidBingoSizeException e) {
+            }
+        }
+    }
+
+    private static BingoType createBingoType() throws IOException {
+        while (true) {
+            OutputView.printRequestForBingoType();
+            String playerAnswer = InputView.requestPlayerAnswer();
+            try {
+                return BingoType.from(playerAnswer);
+            } catch (InvalidBingoTypeException e) {
             }
         }
     }
