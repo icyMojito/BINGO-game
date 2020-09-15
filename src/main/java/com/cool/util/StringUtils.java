82b7331b9611e6190d799230a472bc302cc0627b
icyMojito <dev.icymojito@gmail.com>
2020-09-15 22:46:10 +0900

    feat: String 형태 변수의 검증을 담당하는 StringUtils 생성

---
+++ src/main/java/com/cool/util/StringUtils.java
@@ -0,0 +1,27 @@
+package com.cool.util;
+
+import java.util.Optional;
+import java.util.function.Supplier;
+
+public class StringUtils {
+
+    private StringUtils() {
+    }
+
+    public static void validateNonNullAndNotEmpty(String value) {
+        Optional.ofNullable(value)
+                .filter(v -> !v.isEmpty())
+                .orElseThrow(() -> new IllegalArgumentException("null 혹은 빈 값이 매개변수로 들어왔습니다!"));
+    }
+
+    public static void validateNumberFormat(Supplier<RuntimeException> exceptionSupplier, String value) {
+        validateNonNullAndNotEmpty(value);
+
+        for (int i = 0; i < value.length(); i++) {
+            char c = value.charAt(i);
+            if (c < '0' || '9' < c || (c == '0' && i == 0)) {
+                throw exceptionSupplier.get();
+            }
+        }
+    }
+}
