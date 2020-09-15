7da716b14fb9f50faa188c1dec52e6b3fe6e03da
icyMojito <dev.icymojito@gmail.com>
2020-09-15 22:50:29 +0900

    feat: 빙고 사이즈를 관리하는 BingoSize 생성
    BingoLineCount라는 클래스 이름은 행(row)을 나타내는 성격이 큰 이름이므로, 객체의 의도에 맞게 BingoSize로 변경

---
+++ src/main/java/com/cool/bingo/BingoSize.java
@@ -0,0 +1,63 @@
+package com.cool.bingo;
+
+import com.cool.exception.InvalidBingoSizeException;
+import com.cool.util.StringUtils;
+
+import java.util.function.Supplier;
+
+public class BingoSize {
+    private static final int MIN_VALUE = 3;
+    private static final int MAX_VALUE = 8;
+
+    private final int value;
+
+    private BingoSize(int value) {
+        this.value = value;
+    }
+
+    public static BingoSize from(String bingoSize) {
+        StringUtils.validateNonNullAndNotEmpty(bingoSize);
+        validateNumberFormat(bingoSize);
+        int value = Integer.parseInt(bingoSize);
+        validateNumberRange(value);
+
+        return new BingoSize(value);
+    }
+
+    private static void validateNumberFormat(String bingoSize) {
+        Supplier<RuntimeException> exceptionSupplier =
+                () -> new InvalidBingoSizeException("빙고판의 크기로 숫자가 아닌 값이 들어왔습니다 -> " + bingoSize);
+        StringUtils.validateNumberFormat(exceptionSupplier, bingoSize);
+    }
+
+    private static void validateNumberRange(int bingoSize) {
+        if (bingoSize < MIN_VALUE || MAX_VALUE < bingoSize) {
+            StringBuilder st = new StringBuilder();
+            st.append("빙고판 크기인 숫자가 정해진 범위를 넘었습니다. -> ")
+                    .append(bingoSize)
+                    .append(" / 최소값 -> ")
+                    .append(MIN_VALUE)
+                    .append(", 최대값 -> ")
+                    .append(MAX_VALUE);
+
+            throw new InvalidBingoSizeException(st.toString());
+        }
+    }
+
+    public BingoNumber[][] createCells() {
+        return new BingoNumber[this.value][this.value];
+    }
+
+    public boolean isBiggerThan(int lineOrder) {
+        return lineOrder < this.value;
+    }
+
+    public boolean isBiggerThanMaxNumber(int number) {
+        int maxNumber = (int) Math.pow(this.value, 2) * 2;
+        return maxNumber < number;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
