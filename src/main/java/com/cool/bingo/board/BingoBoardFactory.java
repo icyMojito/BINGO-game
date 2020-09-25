fe5cd079b51b234e92f1f8dc263ea47673268628
icyMojito <dev.icymojito@gmail.com>
2020-09-25 23:11:00 +0900

    feat: BingoBoardFactory 생성

---
+++ src/main/java/com/cool/bingo/board/BingoBoardFactory.java
@@ -0,0 +1,16 @@
+package com.cool.bingo.board;
+
+import com.cool.bingo.BingoSize;
+import com.cool.bingo.BingoType;
+import com.cool.bingo.PlayerType;
+import com.cool.bingo.number.BingoNumbers;
+
+public class BingoBoardFactory {
+    public static BingoBoard createBingoBoard(BingoSize bingoSize, BingoType bingoType, PlayerType playerType,
+                                              BingoNumbers bingoNumbers) {
+        if (BingoType.BLACK == bingoType) {
+            return BlackBingoBoard.of(bingoSize, bingoType, playerType, bingoNumbers);
+        }
+        return NumberBingoBoard.of(bingoSize, bingoType, playerType, bingoNumbers);
+    }
+}
