643e0ba73281ebf9ac06e4770b0af9681be124d0
icyMojito <dev.icymojito@gmail.com>
2020-09-26 22:41:00 +0900

    feat: BingoGameApplication에 빙고 게임 전체 구현

--- src/main/java/com/cool/BingoGameApplication.java
+++ src/main/java/com/cool/BingoGameApplication.java
@@ -1,34 +1,70 @@
 package com.cool;
 
-import com.cool.bingo.BingoNumber;
-import com.cool.bingo.BingoNumbers;
 import com.cool.bingo.BingoSize;
 import com.cool.bingo.BingoType;
+import com.cool.bingo.PlayerType;
 import com.cool.bingo.board.BingoBoard;
-import com.cool.bingo.board.ComputerBingoBoard;
-import com.cool.bingo.board.UserBingoBoard;
-import com.cool.exception.InvalidBingoNumberException;
+import com.cool.bingo.board.BingoBoardFactory;
+import com.cool.bingo.number.BingoNumber;
+import com.cool.bingo.number.BingoNumbers;
 import com.cool.view.InputView;
 import com.cool.view.OutputView;
 
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.BooleanSupplier;
 
 public class BingoGameApplication {
     public static void main(String[] args) throws IOException {
         BingoSize bingoSize = createBingoSize();
         BingoType bingoType = createBingoType();
 
-        UserBingoBoard userBingoBoard = new UserBingoBoard(bingoSize);
-        addNumbersToUserBingoBoard(userBingoBoard, bingoSize);
+        BingoNumbers userBingoNumbers = createBingoNumbers(bingoSize);
+        BingoBoard userBingoBoard = BingoBoardFactory.createBingoBoard(bingoSize, bingoType, PlayerType.USER,
+                                                                       userBingoNumbers);
 
-        ComputerBingoBoard computerBingoBoard = new ComputerBingoBoard(bingoSize);
+        BingoNumbers computerBingoNumbers = BingoNumbers.createRandomBingoNumbers(bingoSize);
+        BingoBoard computerBingoBoard = BingoBoardFactory.createBingoBoard(bingoSize, bingoType, PlayerType.COMPUTER,
+                                                                           computerBingoNumbers);
 
-        removeNumbersInBingoBoard(bingoSize, userBingoBoard, computerBingoBoard);
+        List<BingoBoard> bingoBoards = Arrays.asList(userBingoBoard, computerBingoBoard);
+
+        while (true) {
+            if (isBingoAfterNumberMark(bingoBoards)) {
+                break;
+            }
+        }
 
         OutputView.printGameResult(userBingoBoard);
+    }
+
+    private static boolean isBingoAfterNumberMark(List<BingoBoard> bingoBoards) throws IOException {
+        for (BingoBoard bingoBoard : bingoBoards) {
+            BingoNumber bingoNumberToMark = bingoBoard.pickBingoNumber();
+
+            markNumberInBingoBoards(bingoBoards, bingoNumberToMark);
+
+            if (isAnyBingoInBingoBoards(bingoBoards)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static void markNumberInBingoBoards(List<BingoBoard> bingoBoards, BingoNumber bingoNumberToMark) throws IOException {
+        for (BingoBoard board : bingoBoards) {
+            board.mark(bingoNumberToMark);
+        }
+    }
+
+    private static boolean isAnyBingoInBingoBoards(List<BingoBoard> bingoBoards) throws IOException {
+        for (BingoBoard bingoBoard : bingoBoards) {
+            if (bingoBoard.isBingo()) {
+                OutputView.printWinBingoBoard(bingoBoard.getBingoNumbers(), bingoBoard.getPlayerType());
+                return true;
+            }
+        }
+        return false;
     }
 
     private static BingoSize createBingoSize() throws IOException {
@@ -53,64 +89,26 @@ public class BingoGameApplication {
         }
     }
 
-    private static void addNumbersToUserBingoBoard(UserBingoBoard userBingoBoard, BingoSize bingoSize) throws IOException {
+    private static BingoNumbers createBingoNumbers(BingoSize bingoSize) throws IOException {
         OutputView.printRequestForBingoNumber(bingoSize);
 
-        while (userBingoBoard.hasNeedMoreNumbers()) {
+        BingoNumbers bingoNumbers = BingoNumbers.empty();
+        while (bingoSize.isBiggerThan(bingoNumbers.getSize())) {
             OutputView.printRequestForNumberInput();
             String bingoNumbersValue = InputView.requestPlayerInput();
-            try {
-                BingoNumbers bingoNumbers = BingoNumbers.of(bingoNumbersValue, bingoSize);
-                userBingoBoard.addNumbers(bingoNumbers);
-                OutputView.printBingoBoard(bingoSize, userBingoBoard.getBingoNumbers());
-            } catch (InvalidBingoNumberException e) {
-                OutputView.printExceptionMessage(e);
-                OutputView.printRequestForBingoNumber(bingoSize);
+
+            BingoNumbers numbers = BingoNumbers.of(bingoNumbersValue, bingoSize);
+            BingoNumbers newBingoNumbers = bingoNumbers.add(numbers);
+
+            if (bingoSize.isSmallerThan(newBingoNumbers.getSize())) {
+                newBingoNumbers = newBingoNumbers.reduce(bingoSize);
             }
+
+            bingoNumbers = newBingoNumbers;
+
+            OutputView.printBingoNumbers(bingoNumbers, bingoSize);
         }
-    }
 
-    private static void removeNumbersInBingoBoard(BingoSize bingoSize,
-                                                  UserBingoBoard userBingoBoard,
-                                                  ComputerBingoBoard computerBingoBoard) throws IOException {
-        List<BingoBoard> boards = Arrays.asList(userBingoBoard, computerBingoBoard);
-        BooleanSupplier hasAnyBlackBingo = () -> boards.stream().anyMatch(BingoBoard::isBlackBingo);
-
-        while (true) {
-            OutputView.printRequestToMarkBingoNumber(bingoSize);
-            String bingoNumberToMarkValue = InputView.requestPlayerInput();
-            BingoNumber bingoNumberToMark;
-            try {
-                bingoNumberToMark = BingoNumber.of(bingoNumberToMarkValue, bingoSize);
-            } catch (IllegalArgumentException e) {
-                OutputView.printNoticeForEmptyBingoNumber();
-                continue;
-            }
-
-            markBingoNumber(userBingoBoard, computerBingoBoard, bingoNumberToMark, bingoSize);
-
-            if (hasAnyBlackBingo.getAsBoolean()) {
-                break;
-            }
-
-            BingoNumber computerBingoNumberToMark = computerBingoBoard.getBingoNumberToRemove();
-            OutputView.printComputerBingoNumberToMark(computerBingoNumberToMark);
-
-            markBingoNumber(userBingoBoard, computerBingoBoard, computerBingoNumberToMark, bingoSize);
-
-            if (hasAnyBlackBingo.getAsBoolean()) {
-                break;
-            }
-        }
-    }
-
-    private static void markBingoNumber(UserBingoBoard userBingoBoard, ComputerBingoBoard computerBingoBoard,
-                                        BingoNumber bingoNumberToMark, BingoSize bingoSize) throws IOException {
-        userBingoBoard.markBingoNumber(bingoNumberToMark);
-        computerBingoBoard.markBingoNumber(bingoNumberToMark);
-        OutputView.printBingoBoard(bingoSize, userBingoBoard.getBingoNumbers());
-
-        System.out.println("🍁컴퓨터용 빙고판🍁");
-        OutputView.printBingoBoard(bingoSize, computerBingoBoard.getBingoNumbers());
+        return bingoNumbers;
     }
 }
